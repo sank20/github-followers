69487b2cf498e5776385b15a2c6bf3860fcdbff0
sank20 <sanketpimple@gmail.com>
2023-03-25 16:06:54 -0700

    Initial commit - programmatic setup and icons

---
+++ github-followers/github-followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-followers
+//
+//  Created by Sanket Pimple on 3/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
