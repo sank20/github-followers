07eccd639216a20785032c3942b642e75029e486
sank20 <sanketpimple@gmail.com>
2023-03-27 16:54:55 -0700

    Add code to Download images and cache them

--- github-followers/github-followers/Managers/NetworkManager.swift
+++ github-followers/github-followers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Sanket Pimple on 3/26/23.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
     private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
